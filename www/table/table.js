8e793729957b88cb939a34847add09503d0cb249
Olexandr Nazarenko <nazarenko@samsonos.com>
2014-12-25 13:00:06 +0200

    Made js universal to support nested modules

--- www/table/table.js
+++ www/table/table.js
@@ -1,5 +1,5 @@
 /** Initialization of materials table */
-s('#material').pageInit(function()
+s('.material-table').pageInit(function()
 {		
 	/** Generic SamsonCMS table  initialization */
 	SamsonCMSTable(s('.material-table'), s('.table-pager'));
