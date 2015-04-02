8cca0bd8e7524325eab48984faea27aafa78a892
omaximus <maxomelch93@gmail.com>
2015-04-02 18:13:21 +0300

    new fields are used

--- src/FormFieldTable.php
+++ src/FormFieldTable.php
@@ -5,7 +5,6 @@ use samson\activerecord\dbRelation;
 use samson\activerecord\dbConditionGroup;
 use samson\activerecord\dbConditionArgument;
 use samson\core\SamsonLocale;
-use samson\cms\input\Field;
 use samson\activerecord\dbQuery;
 use samson\pager\Pager;
 use samson\activerecord\dbMySQLConnector;
@@ -18,56 +17,61 @@ use samson\activerecord\dbMySQLConnector;
  */
 class FormFieldTable extends \samson\cms\table\Table
 {
-	/** Change table template */
-	public $table_tmpl = 'form/fieldtable/tmpl';
+    /** Change table template */
+    public $table_tmpl = 'form/fieldtable/tmpl';
 
-	/** Default table row template */
-	public $row_tmpl = 'form/fieldtable/row/index';
-		
-	/** Existing CMSMaterial field records */
-	private $materialfields = array();
-	
-	/** Pointer to CMSMaterial */
-	private $db_material;
-	
-	/** Pointer to Form */
-	private $form;
-	
-	/** Fields locale */
-	private $locale;	
-	
-	//public $debug = true;
+    /** Default table row template */
+    public $row_tmpl = 'form/fieldtable/row/index';
 
-	/**
-	 * Constructor
-	 * @param CMSMaterial 	$db_material 	CMSMaterial pointer
-	 * @param string 		$locale			Field table locale	 
-	 */
-	public function __construct( \samson\cms\CMSMaterial & $db_material, Form & $form, $locale = SamsonLocale::DEF  )
-	{		
-		$this->locale = $locale;
-		
-		// Save pointer to Form
-		$this->form = & $form;
-		
-		// Save pointer to CMSMaterial
-		$this->db_material = & $db_material;
+    /** Existing CMSMaterial field records */
+    private $materialfields = array();
 
-		// Prepare db query for all related material fields to structures
-		$this->query = dbQuery( 'samson\cms\CMSNavField')
-			->cond(dbMySQLConnector::$prefix.'field_Type', array('8', '5'), dbRelation::NOT_EQUAL)
+    /** Pointer to CMSMaterial */
+    private $db_material;
+
+    /** Pointer to Form */
+    private $form;
+
+    /** Fields locale */
+    private $locale;
+
+    public $dbQuery;
+
+    //public $debug = true;
+
+    /**
+     * Constructor
+     * @param CMSMaterial $db_material CMSMaterial pointer
+     * @param string $locale Field table locale
+     */
+    public function __construct(\samson\cms\CMSMaterial & $db_material, Form & $form, $locale = SamsonLocale::DEF)
+    {
+        $this->locale = $locale;
+
+        $this->dbQuery = new dbQuery();
+
+        // Save pointer to Form
+        $this->form = &$form;
+
+        // Save pointer to CMSMaterial
+        $this->db_material = &$db_material;
+
+        // Prepare db query for all related material fields to structures
+        $this->query = dbQuery('samson\cms\CMSNavField')
+            ->cond(dbMySQLConnector::$prefix . 'field_Type', array('8', '5'), dbRelation::NOT_EQUAL)
             ->join('samson\cms\CMSField')
-            ->group_by(dbMySQLConnector::$prefix.'field_FieldID')
-			->order_by('FieldID', 'ASC')
-			->Active(1);
+            ->group_by(dbMySQLConnector::$prefix . 'field_FieldID')
+            ->order_by('FieldID', 'ASC')
+            ->Active(1);
 
         // Get all related structures and remove them from request
         $relatedStructureIDs = dbQuery('structure')->cond('type', 1)->fields('StructureID');
 
         if ($db_material->type != 0 && sizeof($relatedStructureIDs)) {
-            $fieldsIDs = dbQuery('structurefield')->cond('StructureID',$relatedStructureIDs)->fields('FieldID');
+            $fieldsIDs = dbQuery('structurefield')->cond('StructureID', $relatedStructureIDs)->fields('FieldID');
             if (sizeof($fieldsIDs)) {
-                $this->query->cond('FieldID', $fieldsIDs, dbRelation::NOT_EQUAL); // Remove related structure fields from field table
+                // Remove related structure fields from field table
+                $this->query->cond('FieldID', $fieldsIDs, dbRelation::NOT_EQUAL);
             }
         }
 
@@ -78,106 +82,102 @@ class FormFieldTable extends \samson\cms\table\Table
                 unset($navs[$key]);
             }
         }
-		// If material has related structures add them to query
-		$structureIDs = array_keys($navs);
-		if(sizeof($structureIDs)) {
+        // If material has related structures add them to query
+        $structureIDs = array_keys($navs);
+        if (sizeof($structureIDs)) {
             $this->query->StructureID($structureIDs);
         }
-		
-		// Create materialfield db query
-		$mfQuery = dbQuery('materialfield')	
-			->locale($locale)
-			->MaterialID($db_material->id)
-			->Active(1);
-		
-		// Add localization condition if necessary
-		if ($locale != '') { // Show only localizable fields
-			$this->query->cond(dbMySQLConnector::$prefix.'field_local', 1);		
-		} else { // Show only not localizable fields
-            $this->query->cond(dbMySQLConnector::$prefix.'field_local', 0);
+
+        // Create materialfield db query
+        $mfQuery = dbQuery('materialfield')
+            ->locale($locale)
+            ->MaterialID($db_material->id)
+            ->Active(1);
+
+        // Add localization condition if necessary
+        if ($locale != '') { // Show only localizable fields
+            $this->query->cond(dbMySQLConnector::$prefix . 'field_local', 1);
+        } else { // Show only not localizable fields
+            $this->query->cond(dbMySQLConnector::$prefix . 'field_local', 0);
         }
-		
-		// Perform DB request to find existing materialfields
-		//db()->debug(true);
-		if ($mfQuery->exec($db_materialfields)) {
- 			// Recollect materialfields by their identifiers
- 			foreach ( $db_materialfields as $db_materialfield ) 
- 			{
- 				// Save materialfields object by field identifier
- 				$this->materialfields[ $db_materialfield->FieldID ] = $db_materialfield;
-			}
-		}
 
-		//db()->debug(false);
-		
-		//trace('Locale:'.$locale);
-		//trace('Fields:'.implode(',', array_keys($this->materialfields)));
-	
-		// Constructor treed
-		parent::__construct( $this->query );
-	}
-	
-	/** @see \samson\cms\site\Table::row() */
-	public function row(&$db_row, Pager &$pager = null, $module = null)
-	{
-		// Get field metadata
-		$db_field = & $db_row->onetoone['_field'];
-		
-		// If parent Field object not found countinue
-		if( !isset( $db_field ) ) return e('StructureField# ## - Field not found(##)', E_SAMSON_CMS_ERROR, array( $db_row->id, $db_row->FieldID));
+        // Perform DB request to find existing materialfields
+        //db()->debug(true);
+        if ($mfQuery->exec($db_materialfields)) {
+            // Recollect materialfields by their identifiers
+            foreach ($db_materialfields as $db_materialfield) {
+                // Save materialfields object by field identifier
+                $this->materialfields[$db_materialfield->FieldID] = $db_materialfield;
+            }
+        }
 
-		// Try to get already created materialfield object by field id
-		if ( isset($this->materialfields[ $db_row->FieldID ]))	$db_mf = & $this->materialfields[ $db_row->FieldID ];
-		// Otherwise create new material field object
-		else
-		{		
-			//trace('New field for'.$db_field->id);
-			$db_mf = new \samson\activerecord\materialfield(false);
-			$db_mf->locale = $this->locale;
-			$db_mf->Active = 1;
-			$db_mf->MaterialID = $this->db_material->id;
-			$db_mf->FieldID = $db_field->id;
-			$db_mf->save();
-		}
-	
-		// Create input element for field
-		$input = null;
-		
-		// Depending on field type
-		switch( $db_field->Type )
-		{
-			case '4': $input = Field::fromObject( $db_mf, 'Value', 'Select' )->optionsFromString( $db_field->Value ); 	break;
-			case '1': $input = Field::fromObject( $db_mf, 'Value', 'File' );	break;
-			case '3': $input = Field::fromObject( $db_mf, 'Value', 'Date' );	break;
-            case '6': $input = Field::fromObject( $db_mf, 'key_value', 'Material' );	break;
-			case '7': $input = Field::fromObject( $db_mf, 'numeric_value', 'Number' );	break;
-			case '8': return false;//$this->form->tabs[] = new WysiwygTab( $this->form, $db_field, $db_mf, $this->locale ); return false;
-            case '9': return false; break;
-			default: $input = Field::fromObject( $db_mf, 'Value', 'Field' );
-		}	
-		
-		// Render field row
-		return m('material')
-			->view( $this->row_tmpl )
-			->cmsfield( $input )
-			->matfield( $db_mf )
-			->fieldname( isset($db_field->Description{0}) ? $db_field->Description : $db_field->Name )
-			->field( $db_field )
-			->pager( $pager )
-		->output();
-	}
+        //db()->debug(false);
 
-    public function render( array $db_rows = null, $module = null)
+        //trace('Locale:'.$locale);
+        //trace('Fields:'.implode(',', array_keys($this->materialfields)));
+
+        // Constructor treed
+        parent::__construct($this->query);
+    }
+
+    /** @see \samson\cms\site\Table::row() */
+    public function row(&$db_row, Pager &$pager = null, $module = null)
+    {
+        // Get field metadata
+        $db_field = &$db_row->onetoone['_field'];
+
+        // If parent Field object not found countinue
+        if (!isset($db_field)) return e('StructureField# ## - Field not found(##)', E_SAMSON_CMS_ERROR, array($db_row->id, $db_row->FieldID));
+
+        // Try to get already created materialfield object by field id
+        if (isset($this->materialfields[$db_row->FieldID])) $db_mf = &$this->materialfields[$db_row->FieldID];
+        // Otherwise create new material field object
+        else {
+            //trace('New field for'.$db_field->id);
+            $db_mf = new \samson\activerecord\materialfield(false);
+            $db_mf->locale = $this->locale;
+            $db_mf->Active = 1;
+            $db_mf->MaterialID = $this->db_material->id;
+            $db_mf->FieldID = $db_field->id;
+            $db_mf->save();
+        }
+
+        // Create input element for field
+        $input = null;
+
+        // Depending on field type
+        if ($db_field->Type < 8) {
+            // Create input element for field
+            $input = m('samsoncms_input_application')->createFieldByType($this->dbQuery, $db_field->Type, $db_mf);
+        } else {
+            return false;
+        }
+        if ($db_field->Type == 4) {
+            /** @var \samsoncms\input\select\Appliaction $input Input select type */
+            $input->build($db_field->Value);
+        }
+
+        // Render field row
+        return m('material')
+            ->view($this->row_tmpl)
+            ->cmsfield($input)
+            ->matfield($db_mf)
+            ->fieldname(isset($db_field->Description{0}) ? $db_field->Description : $db_field->Name)
+            ->field($db_field)
+            ->pager($pager)
+            ->output();
+    }
+
+    public function render(array $db_rows = null, $module = null)
     {
         // Rows HTML
         $rows = '';
 
         // if no rows data is passed - perform db request
-        if( !isset($db_rows) )	$db_rows = $this->query->exec();
+        if (!isset($db_rows)) $db_rows = $this->query->exec();
 
         // If we have table rows data
-        if( is_array($db_rows ) )
-        {
+        if (is_array($db_rows)) {
             // Save quantity of rendering rows
             $this->last_render_count = sizeof($db_rows);
 
@@ -186,24 +186,22 @@ class FormFieldTable extends \samson\cms\table\Table
             $rc = sizeof($db_rows);
 
             // Iterate db data and perform rendering
-            foreach( $db_rows as & $db_row )
-            {
-                if($this->debug ) elapsed('Rendering row '.$rn++.' of '.$rc.'(#'.$db_row->id.')');
-                $rows .= $this->row( $db_row, $this->pager );
+            foreach ($db_rows as & $db_row) {
+                if ($this->debug) elapsed('Rendering row ' . $rn++ . ' of ' . $rc . '(#' . $db_row->id . ')');
+                $rows .= $this->row($db_row, $this->pager);
                 //catch(\Exception $e){ return e('Error rendering row#'.$rn.' of '.$rc.'(#'.$db_row->id.')'); }
             }
 
-        }
-        // No data found after query, external render specified
-        else $rows .= $this->emptyrow( $this->query, $this->pager );
+        } // No data found after query, external render specified
+        else $rows .= $this->emptyrow($this->query, $this->pager);
 
         //elapsed('render pages: '.$this->pager->total);
 
         // Render table view
         return m($module)
-            ->view( $this->table_tmpl )
-            ->set( $this->pager )
-            ->rows( $rows )
+            ->view($this->table_tmpl)
+            ->set($this->pager)
+            ->rows($rows)
             ->output();
     }
 }
