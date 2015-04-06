10e332fd796dd5be4463895e478d9283d5efa68a
omaximus <maxomelch93@gmail.com>
2015-04-06 19:49:54 +0300

    Removed old table views Added empty and notfound views

--- www/table/table.js
+++
@@ -1,7 +0,0 @@
-/** Initialization of materials table */
-s('.material-table').pageInit(function()
-{		
-	/** Generic SamsonCMS table  initialization */
-	SamsonCMSTable(s('.material-table'), s('.table-pager'));
-});
-
