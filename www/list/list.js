ed52bef1ce31d980b5611ee2a7504da08ec00fd1
Vitaly Egorov <egorov@samsonos.com>
2015-04-10 16:04:30 +0300

    Added new generic list ui js

--- www/list/list.js
+++ www/list/list.js
@@ -2,6 +2,5 @@
  * Created by Maxim Omelchenko on 06.04.2015 at 16:36.
  */
 s('.material-list').pageInit(function(table){
-    SamsonCMSTable(table, s('.table-pager'));
-    //table.fixedHeader();
+    templateList(table, s('.table-pager'));
 });
