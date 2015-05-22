f3b3ccaa5ff3404057fd255c5c0a8152b0331579
Vitaly Iegorov <egorov@samsonos.com>
2015-05-22 18:30:38 +0300

    playing with featurecontext

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -13,18 +13,6 @@ use Behat\MinkExtension\Context\MinkContext;
 class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-
-    }
-
-    /**
      * @Given /^I log out$/
      */
     public function iLogOut() {
