9ca1b09873d6c26057d04605e23a95b693b57acf
Molodyko Ruslan <molodyko@samsonos.com>
2015-09-16 20:10:14 +0300

    Updated module

--- src/form/tab/MaterialField.php
+++ src/form/tab/MaterialField.php
@@ -29,13 +29,6 @@ class MaterialField extends Generic
     {
         $this->name = $field->Description != '' ? $field->Description : $field->Name;
         $this->id .= '_'.$field->Name;
-
-
-
-
-
-
-
         // Prepare locales array with one default locale by default
         $locales = array('');
 
