a4e7af61311a26b87edf9b17c0151ca6b49d2f87
Ruslan <molodyko@samsonos.com>
2015-10-08 16:39:23 +0300

    Added functionality for reload form on change select structure

--- www/form/form.js
+++ www/form/form.js
@@ -1,16 +1,18 @@
 /* Form main JS */
 
-s('.material-structure-selectify').pageInit(function(select) {
+s('.material-structure-selectify').pageInit(selectFunc);
+
+function selectFunc(select) {
     select.selectify();
 
     initLinks(select.prev());
-
 
     s('._sjsselect_dropdown li', select.prev()).each(function(li) {
         if (!li.hasClass('selectify-loaded')) {
             li.click(function(li) {
                 s.ajax(select.a('data-href-add') + '/' + li.a('value'), function(response) {
                     initLinks(select.prev());
+                    updateSelect();
                 });
                 li.addClass('selectify-loaded');
             });
@@ -22,11 +24,54 @@ s('.material-structure-selectify').pageInit(function(select) {
             if (!link.hasClass('selectify-loaded')) {
                 link.click(function(link) {
                     s.ajax(select.a('data-href-remove') + '/' + link.a('value'), function(response) {
+                        time = Date.now();
+                        updateSelect();
                     });
                     link.addClass('selectify-loaded');
                 });
             }
         });
     }
+}
 
-});
+// Global params
+var timer = 0;
+var instance = false;
+var timeout = 500; // 1 sec
+
+// If update select with structures then update main form
+function updateSelect(){
+
+    // If there started some instance of this function then not call any instances
+    if ((instance != false)){
+        return;
+    }
+    instance = true;
+
+    // Set timeout and call loading form
+    setTimeout(function(){
+        loader.show('', true);
+        var changeBlock = s('.application-form');
+        var url = s('.material-structure-selectify').a('data-update-form');
+        s.ajax(url, function(response) {
+            if (response != null) {
+                var data = JSON.parse(response);
+                if (data.form != null) {
+                    // Change hash to main
+                    location.hash = '#samsoncms_form_tab_Generic';
+                    changeBlock.html(data.form);
+                    // Init all tabs
+                    updateTable(s('.material_table_tab'));
+                    initTab();
+                    updateSummernote();
+                    updateWysiwyg(s('.__wysiwyg .__input'));
+                    // Update buttons
+                    SamsonCMS_Input.update(s('body'));
+                    selectFunc(s('.material-structure-selectify'));
+                    loader.hide();
+                    instance = false;
+                }
+            }
+        });
+    }, timeout);
+}
